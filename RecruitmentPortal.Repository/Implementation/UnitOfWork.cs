d2e98cf60cbc565e9681a42d42e48eade9caad5d
KalpTatva <tatva.pca117@outlook.com>
2025-08-14 17:20:36 +0530

    first commit

--- RecruitmentPortal.Repository/Implementation/UnitOfWork.cs
+++ RecruitmentPortal.Repository/Implementation/UnitOfWork.cs
@@ -1,5 +1,7 @@
+using RecruitmentPortal.Repository.Interfaces;
+
 namespace RecruitmentPortal.Repository.Implementation;
 
-public class UnitOfWork
+public class UnitOfWork : IUnitOfWork
 {
 }
--- RecruitmentPortal.Service/Implementation/AuthService.cs
+++ RecruitmentPortal.Service/Implementation/AuthService.cs
@@ -1,5 +1,7 @@
+using RecruitmentPortal.Service.Interfaces;
+
 namespace RecruitmentPortal.Service.Implementation;
 
-public class AuthService
+public class AuthService : IAuthService
 {
 }
